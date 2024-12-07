5b319ff2f9d2aeab69a7ca17db89d888fbf96f0f
Suja16 <sujal16@somaiya.edu>
2024-12-07 13:20:18 +0530

    formate change

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import StatsComponent from "~~/components/StatsComponent";
 import StepComponent from "~~/components/StepComponent";
 import { Address } from "~~/components/scaffold-eth";
-import StatsComponent from "~~/components/StatsComponent";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
@@ -38,12 +38,7 @@ const Home: NextPage = () => {
             <StepComponent currentSteps={2000} totalSteps={6000} />
           </div>
           <div className="flex justify-center my-4">
-            <StatsComponent
-              steps={2259}
-              minutes={23}
-              calories={68}
-              stepsGoal={6000}
-            />
+            <StatsComponent steps={2259} minutes={23} calories={68} stepsGoal={6000} />
           </div>
           <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
             <p className="my-2 font-medium">Connected Address:</p>
