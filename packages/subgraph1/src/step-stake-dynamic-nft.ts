d993dde20979886f5ebc7fe5d8697ea710cb0c5e
Suja16 <sujal16@somaiya.edu>
2024-12-07 05:37:15 +0530

    Initialize subgraph

--- packages/subgraph1/src/step-stake-dynamic-nft.ts
+++ packages/subgraph1/src/step-stake-dynamic-nft.ts
@@ -8,8 +8,8 @@ import {
   StakeForfeit as StakeForfeitEvent,
   Staked as StakedEvent,
   StepsCounted as StepsCountedEvent,
-  Transfer as TransferEvent,
-} from "../generated/StepStakeDynamicNFT/StepStakeDynamicNFT";
+  Transfer as TransferEvent
+} from "../generated/StepStakeDynamicNFT/StepStakeDynamicNFT"
 import {
   Approval,
   ApprovalForAll,
@@ -20,37 +20,37 @@ import {
   StakeForfeit,
   Staked,
   StepsCounted,
-  Transfer,
-} from "../generated/schema";
+  Transfer
+} from "../generated/schema"
 
 export function handleApproval(event: ApprovalEvent): void {
   let entity = new Approval(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.owner = event.params.owner;
-  entity.approved = event.params.approved;
-  entity.tokenId = event.params.tokenId;
+  )
+  entity.owner = event.params.owner
+  entity.approved = event.params.approved
+  entity.tokenId = event.params.tokenId
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
 
-  entity.save();
+  entity.save()
 }
 
 export function handleApprovalForAll(event: ApprovalForAllEvent): void {
   let entity = new ApprovalForAll(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.owner = event.params.owner;
-  entity.operator = event.params.operator;
-  entity.approved = event.params.approved;
+  )
+  entity.owner = event.params.owner
+  entity.operator = event.params.operator
+  entity.approved = event.params.approved
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
 
-  entity.save();
+  entity.save()
 }
 
 export function handleBatchMetadataUpdate(
@@ -58,42 +58,42 @@ export function handleBatchMetadataUpdate(
 ): void {
   let entity = new BatchMetadataUpdate(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity._fromTokenId = event.params._fromTokenId;
-  entity._toTokenId = event.params._toTokenId;
+  )
+  entity._fromTokenId = event.params._fromTokenId
+  entity._toTokenId = event.params._toTokenId
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
 
-  entity.save();
+  entity.save()
 }
 
 export function handleMetadataUpdate(event: MetadataUpdateEvent): void {
   let entity = new MetadataUpdate(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity._tokenId = event.params._tokenId;
+  )
+  entity._tokenId = event.params._tokenId
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
 
-  entity.save();
+  entity.save()
 }
 
 export function handleNFTMinted(event: NFTMintedEvent): void {
   let entity = new NFTMinted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.user = event.params.user;
-  entity.tokenId = event.params.tokenId;
+  )
+  entity.user = event.params.user
+  entity.tokenId = event.params.tokenId
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
 
-  entity.save();
+  entity.save()
 }
 
 export function handleOwnershipTransferred(
@@ -101,70 +101,70 @@ export function handleOwnershipTransferred(
 ): void {
   let entity = new OwnershipTransferred(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.previousOwner = event.params.previousOwner;
-  entity.newOwner = event.params.newOwner;
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
 
-  entity.save();
+  entity.save()
 }
 
 export function handleStakeForfeit(event: StakeForfeitEvent): void {
   let entity = new StakeForfeit(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.user = event.params.user;
-  entity.amount = event.params.amount;
+  )
+  entity.user = event.params.user
+  entity.amount = event.params.amount
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
 
-  entity.save();
+  entity.save()
 }
 
 export function handleStaked(event: StakedEvent): void {
   let entity = new Staked(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.user = event.params.user;
-  entity.amount = event.params.amount;
+  )
+  entity.user = event.params.user
+  entity.amount = event.params.amount
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
 
-  entity.save();
+  entity.save()
 }
 
 export function handleStepsCounted(event: StepsCountedEvent): void {
   let entity = new StepsCounted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.user = event.params.user;
-  entity.stepCount = event.params.stepCount;
+  )
+  entity.user = event.params.user
+  entity.stepCount = event.params.stepCount
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
 
-  entity.save();
+  entity.save()
 }
 
 export function handleTransfer(event: TransferEvent): void {
   let entity = new Transfer(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  );
-  entity.from = event.params.from;
-  entity.to = event.params.to;
-  entity.tokenId = event.params.tokenId;
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.tokenId = event.params.tokenId
 
-  entity.blockNumber = event.block.number;
-  entity.blockTimestamp = event.block.timestamp;
-  entity.transactionHash = event.transaction.hash;
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
 
-  entity.save();
+  entity.save()
 }
