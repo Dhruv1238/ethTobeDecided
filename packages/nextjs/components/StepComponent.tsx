41e8a812bf10d3186c8ae54dd5f0cf1a3a436e8c
Harshal Dulera <harshaldulera82@gmail.com>
2024-12-07 13:13:40 +0530

    feat: step component

--- packages/nextjs/components/StepComponent.tsx
+++ packages/nextjs/components/StepComponent.tsx
@@ -11,14 +11,14 @@ const StepComponent: FC<StepComponentProps> = ({ currentSteps, totalSteps }) => 
   return (
     <div className="flex items-center justify-between w-full rounded-xl bg-[#2d2c2e] p-4">
       <div className="flex flex-col">
-        <span className="text-[#fbf8fe] text-2xl font-bold">
+        <span className="text-[#fbf8fe] text-4xl font-bold">
           {currentSteps.toLocaleString()}
         </span>
-        <span className="text-[#a3a2a7] text-sm">/{totalSteps.toLocaleString()} steps</span>
+        <span className="text-[#a3a2a7] text-lg">/{totalSteps.toLocaleString()} steps</span>
       </div>
       
       <div className="relative flex items-center">
-        <div className="relative w-32 h-2 bg-[#000001] rounded-full overflow-hidden">
+        <div className="relative w-32 h-3 bg-[#000001] rounded-full overflow-hidden">
           <div 
             className="absolute top-0 left-0 h-full bg-[#11ce6f] rounded-full transition-all duration-300"
             style={{ width: `${percentage}%` }}
